a702a90056735c3553a07acd1e2094d417271a91
Buzzeh <benitez@vt.edu>
2019-05-07 11:41:32 -0400

    I got you baby

---
+++ betterdep/config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: 'e9a1fadb155a3f96c048',
+  GITHUB_CLIENT_SECRET: '6e126fb890cd33ab048a879bb597480b389ffb17',
+  GITHUB_CALLBACK_URL: 'http://localhost:3030/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/test',
+};
